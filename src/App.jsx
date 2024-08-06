f44b1384db20bb5a67b36f2e45dcefc062e29f19
AB7zz <abhinavcv007@gmail.com>
2024-08-06 19:26:10 +0530

    tshirts responses

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey:  import.meta.env.VITE_apiKey,
-  authDomain:  import.meta.env.VITE_authDomain,
-  projectId:  import.meta.env.VITE_projectId,
-  storageBucket:  import.meta.env.VITE_storageBucket,
-  messagingSenderId:  import.meta.env.VITE_messagingSenderId,
-  appId:  import.meta.env.VITE_appId,
-  measurementId:  import.meta.env.VITE_measurementId
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
@@ -22,7 +22,7 @@ function App() {
   const [usersCopy, setUsersCopy] = React.useState([])
   React.useEffect(() => {
     async function fetchRegisteredUsers() {
-      const usersCollection = collection(db, 'registeredUsers');
+      const usersCollection = collection(db, 'orders');
       
       try {
         const querySnapshot = await getDocs(usersCollection);
@@ -48,34 +48,53 @@ function App() {
     if(e.target.value == ''){
       setUsersCopy(users)
     }else{
-      setUsersCopy(users.filter(user => user.formData.code.toLowerCase().includes(e.target.value)))
+      setUsersCopy(users.filter(user => user.token.toLowerCase().includes(e.target.value)))
     }
   }
   const handleName = (e) => {
     if(e.target.value == ''){
       setUsersCopy(users)
     }else{
-      setUsersCopy(users.filter(user => user.formData.name.toLowerCase().includes(e.target.value)))
+      setUsersCopy(users.filter(user => user.name.toLowerCase().includes(e.target.value)))
     }
   }
 
   return (
-    <div className='px-10 py-10'>
+    <div className='bg-zinc-800 px-10 py-10'>
       <div className='flex justify-evenly'>
         <input onChange={e => handleCode(e)} type="text" placeholder='search by code' className='pl-3 py-3 rounded-[10px] border-[1.5px] border-zinc-300' />
         <input onChange={e => handleName(e)} type="text" placeholder='search by name' className='pl-3 py-3 rounded-[10px] border-[1.5px] border-zinc-300' />
       </div>
+      {/* <div className='my-10 bg-zinc-900 px-10 py-5 rounded-[15px]'>
+        <h1 className='text-red-500 text-xl'><span className='font-bold'>Code: </span>#asdasdf</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>Abhinav C V</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Phone: </span>+919778393558</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>abhinavcv007@gmail.com  </h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>from CUSAT: </span>Yes</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>from SEDS: </span>Yes</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Year of study: </span>2025</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Referral Code: </span>None</h1>
+        <img src='/vite.svg' className='w-[150px]' alt="" />
+      </div>
+      <div className='my-10 bg-zinc-900 px-10 py-5 rounded-[15px]'>
+        <h1 className='text-red-500 text-xl'><span className='font-bold'>Code: </span>#asdasdf</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>Abhinav C V</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Phone: </span>+919778393558</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>abhinavcv007@gmail.com  </h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>from CUSAT: </span>Yes</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>from SEDS: </span>Yes</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Year of study: </span>2025</h1>
+        <h1 className='text-white text-xl'><span className='font-bold'>Referral Code: </span>None</h1>
+        <img src='/vite.svg' className='w-[150px]' alt="" />
+      </div> */}
       {usersCopy && usersCopy.map(user => (
         <div className='my-10 bg-zinc-900 px-10 py-5 rounded-[15px]' key={user.id}>
-          <h1 className='text-red-500 text-xl'><span className='font-bold'>Code: </span>{user.formData.code || 'None'}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>{user.formData.name}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>Phone: </span>{user.formData.phone}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>{user.formData.email}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>from CUSAT: </span>{user.formData.isFromCUSAT}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>from SEDS: </span>{user.formData.isSEDSMember}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>Year of study: </span>{user.formData.yearOfStudy}</h1>
-          <h1 className='text-white text-xl'><span className='font-bold'>Referral Code: </span>{user.formData.referralCode || 'None'}</h1>
-          <img src={user.formData.file} className='w-[150px]' alt="" />
+          <h1 className='text-red-500 text-xl'><span className='font-bold'>Token: </span>{user.token || 'None'}</h1>
+          <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>{user.name}</h1>
+          <h1 className='text-white text-xl'><span className='font-bold'>Phone: </span>{user.phone}</h1>
+          <h1 className='text-white text-xl'><span className='font-bold'>Name: </span>{user.email}</h1>
+          <h1 className='text-white text-xl'><span className='font-bold'>from CUSAT: </span>{user.cusatian}</h1>
+          <img src={user.paymentScreenshot} className='w-[150px]' alt="" />
         </div>
       ))
       }
