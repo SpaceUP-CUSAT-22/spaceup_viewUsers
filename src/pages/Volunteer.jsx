dac1665a164b0fc4105afd5a962f5466d04bf235
evin <evinjohnignatious@gmail.com>
2024-09-25 00:25:12 +0530

    fixed

--- src/pages/Volunteer.jsx
+++ src/pages/Volunteer.jsx
@@ -4,7 +4,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { Link } from 'react-router-dom';
 
 const firebaseConfig = {
-  // Your Firebase configuration details
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
